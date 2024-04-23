f37b6aa198d7bad516680a0e5cef4f6305ea6929
KimMundo01 <801sml@gmail.com>
2024-04-23 13:36:47 +0900

    MukGenie BackEnd

---
+++ src/main/java/org/example/mukgenie/MukGenieApplication.java
@@ -0,0 +1,13 @@
+package org.example.mukgenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukGenieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MukGenieApplication.class, args);
+    }
+
+}
