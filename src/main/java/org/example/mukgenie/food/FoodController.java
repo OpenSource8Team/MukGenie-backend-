e57cc86a62b78b957048470d4263cfbbc7ad5ed8
KimMundo01 <801sml@gmail.com>
2024-05-09 09:27:35 +0900

    메세지 체인지

--- src/main/java/org/example/mukgenie/food/FoodController.java
+++ src/main/java/org/example/mukgenie/food/FoodController.java
@@ -16,25 +16,25 @@ public class FoodController {
         this.foodService = foodService;
     }
 
-    // 새로운 사용자 생성
+    // 새로운 음식 생성
     @PostMapping
     public Food createFood(@RequestBody Food food) {
         return foodService.createFood(food);
     }
 
-    // 모든 사용자 조회
+    // 모든 음식 조회
     @GetMapping
     public List<Food> getAllFoods() {
         return foodService.getAllFoods();
     }
 
-    // 특정 ID를 가진 사용자 조회
+    // 특정 ID를 가진 음식 조회
     @GetMapping("/{id}")
     public Food getFoodById(@PathVariable String id) {
         return foodService.getFoodById(id);
     }
 
-    // 특정 ID를 가진 사용자 삭제
+    // 특정 ID를 가진 음식 삭제
     @DeleteMapping("/{id}")
     public void deleteFoodById(@PathVariable String id) {
         foodService.deleteFoodById(id);
