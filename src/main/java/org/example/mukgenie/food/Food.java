1602b070392d2e9554f5da11a5e9caae4f94dcd1
KimMundo1 <801sml@gmail.com>
2024-05-18 20:46:54 +0900

    음식 타입 추가

--- src/main/java/org/example/mukgenie/food/Food.java
+++ src/main/java/org/example/mukgenie/food/Food.java
@@ -13,25 +13,20 @@ public class Food {
 
     @Id
     private String id; // 사용자 ID
-
     private String name;//이름
-
     private String country; //나라
-
     private Integer personnel; //인원
-
     private Integer price; //가격
-
     private String type; //종류(면, 빵, 밥)
-
     private String allergy;//알러지
-
     private String cook;// 조리타입(굽기,삶기,찌기)
-
     private String dlike;// 못 먹는 음식
+    private Integer calorie; //칼로리
 
-    private Integer calorie;
-
+    private Boolean temperature;//온도
+    private Boolean spicy; //맵기
+    private Boolean soup; //국물
+    private Boolean oil; //기름
 
     // 생성자, getter, setter는 생략되어 있습니다.
 }
