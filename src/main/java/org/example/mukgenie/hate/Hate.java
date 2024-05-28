b15d24d8cd4dd5df91baea1faac74e2b2330ac4c
KimMundo01 <801sml@gmail.com>
2024-05-28 16:56:07 +0900

    의사결정트리 예제 추가

---
+++ src/main/java/org/example/mukgenie/hate/Hate.java
@@ -0,0 +1,22 @@
+package org.example.mukgenie.hate;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+// 사용자 정보를 나타내는 모델 클래스
+@Document(collection = "Hate")
+@Getter
+@Setter
+public class Hate {
+
+    @Id
+    private String id; // 사용자 ID
+    private String name; // 이름
+    private List<String> foods; // 싫어하는 음식 목록
+
+    // 생성자, getter, setter는 Lombok에 의해 자동 생성됩니다.
+}
